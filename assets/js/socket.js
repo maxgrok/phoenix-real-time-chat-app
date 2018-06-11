9f1f5dbab4de64348e44932ee88334ec9c2c10af
Max Goodman <max.goodman@flatironschool.com>
2018-06-11 15:10:12 -0400

    first commit

---
+++ assets/js/socket.js
@@ -0,0 +1,28 @@
+// NOTE: The contents of this file will only be executed if
+// you uncomment its entry in "assets/js/app.js".
+
+// To use Phoenix channels, the first step is to import Socket
+// and connect at the socket path in "lib/web/endpoint.ex":
+let socket = new Socket("/socket", {params: {token: window.userToken}})
+let channel = socket.channel("lobby", {});
+let list    = $('#message-list');
+let message = $('#message');
+let name    = $('#name');
+
+message.on('keypress', event => {
+  if (event.keyCode == 13) {
+    channel.push('new_message', { name: name.val(), message: message.val() });
+    message.val('');
+  }
+});
+
+channel.on('new_message', payload => {
+  list.append(`<b>${payload.name || 'Anonymous'}:</b> ${payload.message}<br>`);
+  list.prop({scrollTop: list.prop("scrollHeight")});
+});
+
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+socket.connect()
+
